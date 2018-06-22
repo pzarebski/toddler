49c482fa34bf573ad2c2e95b9ce78759085d4361
pzarebski <pzarebski@gmail.com>
2018-06-22 09:07:34 +0200

    assets, js, packages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
+import bootstrap from 'bootstrap'
+import jquery from 'jquery'
+import easing from  'easing'
+import slider from './utils/jquery.flexslider.js'
+import uitotop from './utils/uitotop.js'
+import responsiveslides from 'responsive-slides'
+
+import './assets/css/bootstrap.min.css'
+import './assets/css/style.css'
+
 import App from './App.vue'
 
 new Vue({
